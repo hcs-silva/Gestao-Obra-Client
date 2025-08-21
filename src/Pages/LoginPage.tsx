e0f5760fef524eeab9c8209aa370a9d9af2c70a7
Hernâni Silva <hcsilvamatos@hotmail.com>
2025-08-21 22:12:18 +0100

    Add .env file for backend URL configuration; update package.json and package-lock.json to include axios and dotenv dependencies; implement login functionality in LoginPage component

--- src/Pages/LoginPage.tsx
+++ src/Pages/LoginPage.tsx
@@ -1,20 +1,51 @@
 import { useState } from "react";
 import styles from "../styles/loginpage.module.css";
+import axios from "axios";
+
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:5005";
+
 const LoginPage = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
+  async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    const userToLogin = {
+      username,
+      password,
+    };
+
+      //TODO: Finish implementing login workflow here and in the server and role-based authentication
+
+    axios
+      .post(`${BACKEND_URL}/users/login`, userToLogin).then((response) => {
+        localStorage.setItem("token", response.data.authToken);
+        localStorage.setItem("userId", response.data.userId)
+      })
+      
+  }
+
   return (
     <div className={styles.loginpage}>
       <h1 className={styles.h1}>Login</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>
           Username:
-          <input type="text" name="username" value={username} onChange={(e)=> setUsername(e.target.value)}/>
+          <input
+            type="text"
+            name="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
         </label>
         <label>
           Password:
-          <input type="password" name="password" value={password} onChange={(e)=> setPassword(e.target.value)}/>
+          <input
+            type="password"
+            name="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </label>
         <button type="submit" className={styles.button}>
           Login
